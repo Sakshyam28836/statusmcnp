ae6c44a66cf8e04638aba7ad0970ed3fd1bc9358
Sakshyam28836 <paudelmahesh7@gmail.com>
2026-01-26 18:36:20 +0545

    Update Discord invite link in Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -104,7 +104,7 @@ const Index = () => {
 
         {/* Discord + Game Modes */}
         <section className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-8">
-          <DiscordWidget inviteLink="https://discord.gg/mcnp" />
+          <DiscordWidget inviteLink="https://discord.gg/XeC2sMsazu" />
           <GameModeNav />
         </section>
 
